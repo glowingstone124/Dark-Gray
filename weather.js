4aa913ade2680c81da07e441ddde8ba105292756
glowingstone124 <hanserofficial@outlook.com>
2024-03-24 14:45:41 +0800

    remove consolelog

--- weather.js
+++ weather.js
@@ -14,15 +14,12 @@ window.addEventListener('load', () => {
                     document.getElementById('weather-stat').textContent = `${data.weather[0].description}`
                 })
                 .catch(error => {
-                    console.error('获取天气信息时出错：', error);
                     document.getElementById('weather-info').textContent = '无法获取天气信息';
                 });
         }, (error) => {
-            console.error('获取地理位置时出错：', error);
             document.getElementById('weather-info').textContent = '无法获取地理位置';
         });
     } else {
-        console.error('浏览器不支持地理位置 API');
         document.getElementById('weather-info').textContent = '浏览器不支持地理位置 API';
     }
 });
